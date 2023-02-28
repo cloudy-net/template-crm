2fd7d07863808949f1e4f0c17b752c665a168f5f
Björn Ali Göransson <bjorn.a.goransson@gmail.com>
2023-02-28 11:52:59 +0100

    Cleanup, add seed data

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
 using CrmApplication.Models;
-using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -30,8 +29,8 @@ using (var scope = app.Services.CreateScope())
 {
     var context = scope.ServiceProvider.GetService<MyContext>();
 
-    context.Companies.Add(new Company { Id = new Guid("042f0213-6e95-4b23-b924-e43bb51c219d"), Name = "IBM" });
-    context.Companies.Add(new Company { Id = new Guid("51e36eda-12ce-41f7-893e-4a475a2b7116"), Name = "HP" });
+    context.Companies.Add(new Company { Id = new Guid("042f0213-6e95-4b23-b924-e43bb51c219d"), Name = "IBM", Image = "https://cloudytest.blob.core.windows.net/media/logos/ibm.png" });
+    context.Companies.Add(new Company { Id = new Guid("51e36eda-12ce-41f7-893e-4a475a2b7116"), Name = "HP", Image = "https://cloudytest.blob.core.windows.net/media/logos/hp.png" });
 
     context.Customers.Add(new Customer { Id = new Guid("8e4c70f8-a37a-4566-821c-aa30c9e05563"), Name = "John Doe", CompanyId = new Guid("042f0213-6e95-4b23-b924-e43bb51c219d") });
     context.Customers.Add(new Customer { Id = new Guid("bfdacf55-19a0-4f5b-b9c8-4e48cb4f42f5"), Name = "Jane Doe", CompanyId = new Guid("042f0213-6e95-4b23-b924-e43bb51c219d") });
@@ -48,10 +47,8 @@ app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
 
-app.UseEndpoints(endpoints => {
-    endpoints.MapRazorPages();
-    endpoints.MapControllers();
-    endpoints.MapGet("/", async c => c.Response.Redirect("/Admin"));
-});
+app.MapRazorPages();
+app.MapControllers();
+app.MapGet("/", async c => c.Response.Redirect("/Admin"));
 
 app.Run();
